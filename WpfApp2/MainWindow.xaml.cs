51340ac656253a242f935f6fdad094a72efa6ce5
Amira_et65@itstep.edu.az <Amira_et65@itstep.edu.az>
2023-11-15 14:42:18 +0400

    ooo

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,11 +22,37 @@ namespace WpfApp2
     /// </summary>
     public partial class MainWindow : Window
     {
+        int threadnum = 3;
         int a = 1;
+        private List<Thread> CreatedThreadsList;
+        private List<Thread> WaitingThreadsList;
+        private List<Thread> WorkingThreadsList;
+
         public MainWindow()
         {
             InitializeComponent();
             numberTextBlock.Text = $"{a}";
+            DataContext = this;
+            CreatedThreadsList = new List<Thread>();
+
+            for (int i = 0; i < threadnum; i++)
+            {
+                Thread thread = new Thread(new ThreadStart(ThreadMethod));
+                thread.Name = "Thread " + (i + 1);
+                CreatedThreadsList.Add(thread);
+            }
+
+            foreach (Thread thread in CreatedThreadsList)
+            {
+                CreatedListView.Items.Add(new { ThreadName = thread.Name });
+            }
+        }
+
+        private void ThreadMethod()
+        {
+            // Thread görevi
+            Console.WriteLine(Thread.CurrentThread.Name + " is running.");
+            Thread.Sleep(1000);
         }
         private void IncreaseButtonClick(object sender, RoutedEventArgs e)
         {
@@ -34,6 +62,14 @@ namespace WpfApp2
         private void DecreaseButtonClick(object sender, RoutedEventArgs e)
         {
             numberTextBlock.Text = $"{a--}";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Thread thread = new Thread(new ThreadStart(ThreadMethod));
+            thread.Name = "Thread " + threadnum;
+            CreatedThreadsList.Add(thread);
+            threadnum++;
         }
     }
 }
